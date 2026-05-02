e75716272c421e87781343a74103d9320d37ded0
Pedrosilva03 <pedroo.silva180@gmail.com>
2026-05-02 04:49:35 +0100

    Script to fetch and separate frames from a video

---
+++ detector/src/utils/fetchImages.py
@@ -0,0 +1,39 @@
+# Fetch images from a video, adding 80% for training and 20% for testing
+
+import cv2, os, sys, random
+
+portion = 0.8
+
+trainPath = "detector/data/real/images"
+testPath = "detector/data/test"
+
+def getFrames(videoPath):
+    frames = []
+    capture = cv2.VideoCapture(videoPath)
+    success = True
+    counter = 1
+    while success:
+        success, frame = capture.read()
+        if counter % 10 == 0:
+            frames.append(frame)
+        counter += 1
+    return frames
+
+def main(argv):
+    frames = getFrames(argv[1])
+    split_point = int(len(frames) * portion)
+    random.shuffle(frames)
+
+    train = frames[:split_point]
+    test = frames[split_point:]
+
+    frameCounter = 1
+    for trainFrame in train:
+        cv2.imwrite(os.path.join(trainPath, "real_%d.png" % frameCounter), trainFrame)
+
+    frameCounter = 1
+    for testFrame in test:
+        cv2.imwrite(os.path.join(testPath, "real_%d.png" % frameCounter), testFrame)
+
+if __name__ == "__main__":
+    main(sys.argv)
